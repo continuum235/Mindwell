1405f66a3c5819d475a5bf7b66adc3ad7ed0dc6a
continuum235 <tanish123tare@gmail.com>
2026-01-28 15:46:53 +0530

    change encodign

--- frontend/src/pages/MoodTracker.js
+++ frontend/src/pages/MoodTracker.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
